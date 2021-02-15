172e4d93034d24bad4a842445bb7dba160f78707
Maijuriina Mustonen <42972494+maijuriina@users.noreply.github.com>
2021-02-15 12:30:11 +0200

    Added loyalty details toggle and tip amount + picker

--- Views/CheckoutView.swift
+++ Views/CheckoutView.swift
@@ -11,6 +11,10 @@ struct CheckoutView: View {
     @EnvironmentObject var order: Order
     let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
     @State private var paymentType = "Cash" // State watches changes for simple values, and is marked private
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    let tipAmounts = [10, 15, 20, 25, 0]
+    @State private var tipAmount = 15 // add property for storing selected tipAmount
     
     var body: some View {
         Form {
@@ -19,6 +23,26 @@ struct CheckoutView: View {
                     ForEach(paymentTypes, id: \.self) {
                         Text($0)
                     }
+                }
+            }
+            
+            Toggle("Add iDine loyalty card", isOn: $addLoyaltyDetails.animation())
+            if addLoyaltyDetails { // if loyalty has been toggled, show below
+                TextField("Enter your iDine ID", text: $loyaltyNumber)
+            }
+            
+            Section(header: Text("Add a tip?")) {
+                Picker("Percentage:", selection: $tipAmount) { // selection tied to tipAmount
+                    ForEach(tipAmounts, id: \.self) { // set id to own value
+                        Text("\($0)%")
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
+            
+            Section(header: Text("Total: $100")) {
+                Button("Confirm order") {
+                    // place order
                 }
             }
         }
