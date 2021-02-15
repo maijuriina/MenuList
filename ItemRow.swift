e36d6f82e472e506d7053b5afcaf7371436bf534
Maijuriina Mustonen <42972494+maijuriina@users.noreply.github.com>
2021-02-15 10:01:27 +0200

    Food allergens are now displayed with their restriction colours codes using foreach and arrays.

--- ItemRow.swift
+++ ItemRow.swift
@@ -9,16 +9,32 @@ import SwiftUI
 
 struct ItemRow: View {
     let item: MenuItem // introduce MenuItem for calls
+    let colors: [String: Color] = ["D": .purple, "G": .black, "N": .red, "S": .blue, "V": .green] // create dictionary for allergen categories and assign their colours
     
     var body: some View {
         HStack {
             Image(item.thumbnailImage)
+                //.cornerRadius(6.0)
+                .clipShape(Circle())
+            //.overlay(Circle().stroke(Color.gray, lineWidth: 2))
             
             VStack(alignment: .leading) {
                 Text(item.name)
+                    .font(.headline)
                 Text("$\(item.price)")
             }
- 
+            Spacer()
+            
+            ForEach(item.restrictions, id: \.self) { restriction in // loop restrictions for each item using string itself as identifier
+                Text(restriction)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(6)
+                    .background(colors[restriction, default: .black]) // find colors from array
+                    .clipShape(Circle())
+                    .foregroundColor(.white)
+            }
+            
         }
     }
 }
