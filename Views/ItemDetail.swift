a1e503fec142a93563a3c87698e0a71bff5d4dda
Maijuriina Mustonen <42972494+maijuriina@users.noreply.github.com>
2021-02-15 10:23:43 +0200

    ItemDetail styling to fit all screens

--- Views/ItemDetail.swift
+++ Views/ItemDetail.swift
@@ -11,10 +11,24 @@ struct ItemDetail: View {
     let item: MenuItem
     var body: some View {
         VStack {
-            Image(item.mainImage)
+            ZStack(alignment: .bottomTrailing) {
+                Image(item.mainImage)
+                    .resizable()
+                    .scaledToFit() // these two make it so that the image runs edge to edge on all screen sizes
+                Text("Photo: \(item.photoCredit)")
+                    .padding(4)
+                    .background(Color.black)
+                    .font(.caption)
+                    .foregroundColor(.white)
+                    .offset(x: -5, y: -5)
+            }
+
             Text(item.description)
+                .padding()
+            Spacer()
         }
         .navigationTitle(item.name)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
